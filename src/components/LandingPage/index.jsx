370cb249af655811e1f7ba79645eb19ba3f9a469
Bernardo <bernardocardosodacosta@gmail.com>
2021-12-28 16:30:38 -0300

    style: style mobile 99% landing page

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,7 +1,20 @@
-import { MenuItems, Attribution, Container, Header } from "./styles"
+import { MenuItems, Attribution, Container, Header, Section, GraphicSection, PhotographySection, UserComment, Comments, Gallery, Footer } from "./styles"
 
 import Logo from "../../assets/logo.svg"
 import ArrowDown from "../../assets/icon-arrow-down.svg"
+import Egg from "../../assets/mobile/image-transform.jpg"
+import PinkCup from "../../assets/mobile/image-stand-out.jpg"
+import Emily from "../../assets/image-emily.jpg"
+import Thomas from "../../assets/image-thomas.jpg"
+import Jennie from "../../assets/image-jennie.jpg"
+import Milks from "../../assets/mobile/image-gallery-milkbottles.jpg"
+import Orange from "../../assets/mobile/image-gallery-orange.jpg"
+import Cone from "../../assets/mobile/image-gallery-cone.jpg"
+import Sugars from "../../assets/mobile/image-gallery-sugar-cubes.jpg"
+import Facebook from "../../assets/icon-facebook.svg"
+import Instagram from "../../assets/icon-instagram.svg"
+import Twitter from "../../assets/icon-twitter.svg"
+import Pinterest from "../../assets/icon-pinterest.svg"
 
 const LandingPage = () => {
     return (
@@ -20,52 +33,97 @@ const LandingPage = () => {
         <h1>We are creatives</h1>
         <img src={ArrowDown} id="arrow-down" />
         </Header>
+        <Section>
+        <div>
+          <img src={Egg}/>
+        </div>
+        <div className="content">
+          <h2>Transform your brand</h2>
+          <p>
+            We are a full-service creative agency specializing in helping brands grow fast. 
+            Engage your clients through compelling visuals that do most of the marketing for you.
+          </p>
+          <h3>
+          Learn more
+          </h3>
+        </div>
+        </Section>
+        <Section>
+          <div>
+            <img src={PinkCup} />
+          </div>
+          <div>
+            <h2>
+              Stand out to the right audience
+            </h2>
+            <p>
+            Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. 
+            </p>
+            <h3>
+            Learn more
+            </h3>
+          </div>
+        </Section>
+        <GraphicSection>
+        <div>
+          <h2>Graphic design</h2>
+          <p>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.</p>
+        </div>
+        </GraphicSection>
+        <PhotographySection>
+        <div>
+          <h2>Photography</h2>
+          <p>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
+        </div>
+        </PhotographySection>
+        <Comments>
+          <h4>Client testimonials</h4>
+          <UserComment>
+            <img src={Emily}/>
+            <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
+            <h3>Emily R.</h3>
+            <span>Marketing Director</span>
+          </UserComment>
+          <UserComment>
+            <img src={Thomas}/>
+            <p>Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.</p>
+            <h3>Thomas S.</h3>
+            <span>Chief Operating Officer</span>
+          </UserComment>
+          <UserComment>
+          <img src={Jennie}/>
+            <p>Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!</p>
+            <h3>Jennie F.</h3>
+            <span>Business Owner</span>
+          </UserComment>
+        </Comments>
+        <Gallery>
+          <img src={Milks}/>
+          <img src={Orange}/>
+          <img src={Cone}/>
+          <img src={Sugars}/>
+        </Gallery>
+        <Footer>
+          <img src={Logo}/>
+          <div id="footer_links">
+            <span>About</span>
+            <span>Services</span>
+            <span>Projects</span>
+            <span>Contact</span>
+          </div>
+          <div id="footer_icons">
+            <img src={Facebook}/>
+            <img src={Instagram}/>
+            <img src={Twitter}/>
+            <img src={Pinterest}/>
+          </div>
 
-  We are creatives
+          <Attribution>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/bercardosoc" target="_blank" >Bernardo Costa</a>.
+          </Attribution>
 
-  Transform your brand
-
-  We are a full-service creative agency specializing in helping brands grow fast. 
-  Engage your clients through compelling visuals that do most of the marketing for you.
-
-  Learn more
-
-  Stand out to the right audience
-
-  Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. 
-
-  Learn more
-
-  Graphic design
-  Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.
-
-  Photography
-  Increase your credibility by getting the most stunning, high-quality photos that improve your business image.
-
-  Client testimonials
-
-  We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.
-  Emily R.
-  Marketing Director
-
-  Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.
-  Thomas S.
-  Chief Operating Officer
-
-  Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
-  Jennie F.
-  Business Owner
-
-  About
-  Services
-  Projects
-  Contact
-
-  <Attribution>
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Bernardo Costa</a>.
-  </Attribution>
-
+        </Footer>
 </Container>
   )
 }
--- src/components/LandingPage/styles.js
+++ src/components/LandingPage/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import MobileImageHeader from "../../assets/mobile/image-header.jpg"
-
+import Cherry from "../../assets/mobile/image-graphic-design.jpg"
+import Orange from "../../assets/mobile/image-photography.jpg"
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
@@ -12,7 +13,7 @@ export const Header = styled.div`
     background-image: url(${MobileImageHeader});
     background-size: 100%;
     background-repeat: no-repeat;
-    height: 538px;
+    height: 505px;
     padding: 1rem;
 
     h1 {
@@ -29,6 +30,111 @@ export const Header = styled.div`
 export const MenuItems = styled.div`
     display: none;
 `
+export const Section = styled.div`
+    text-align: center;
+
+    p {
+        color: var(--dark-grayish-blue);
+    }
+
+    div, img {
+        width: 100%;
+        height: 23rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+    
+`
+export const GraphicSection = styled.div`
+    height: 550px;
+    background: url(${Cherry});
+    background-size: 100%;
+    background-repeat: no-repeat;
+    text-align: center;
+    color: var(--desatured-cyan);
+
+    div {
+        height: 550px;
+        padding-top: 100%;
+        background-size: 100%;
+        background-repeat: no-repeat;
+        text-align: center;
+    }
+`
+
+export const PhotographySection = styled.div`
+    height: 600px;
+    background: url(${Orange});
+    background-size: 100%;
+    background-repeat: no-repeat;
+    text-align: center;
+    color: var(--dark-blue);
+    div {
+        height: 600px;
+        padding-top: 100%;
+        background-size: 100%;
+        background-repeat: no-repeat;
+        text-align: center;
+    }
+`
+export const Comments = styled.div`
+    text-align: center;
+    color: var(--dark-grayish-blue);
+`
+export const UserComment = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 2.5rem auto;
+    img {
+        width: 25%;
+        border-radius: 50%;
+    }
+    p {
+        color: var(--darkest-grayish-blue);
+        margin: 1rem 2.5rem;
+    }
+    h3 {
+        color: var(--desatured-blue);
+    }
+`
+export const Gallery = styled.div`
+    img {
+        width: 50%;
+    }
+`
+
+export const Footer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 20rem;
+    padding: 2.5rem;
+    background-color: #90d4c5;
+    color: var(--moderate-cyan);
+
+    img {
+        filter: invert(45%) sepia(15%) saturate(1254%) hue-rotate(116deg) brightness(90%) contrast(94%);
+    }
+    
+    #footer_links {
+        display: flex;
+        justify-content: space-around;
+        width: 100%;
+    }
+
+    #footer_icons {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        img {
+            margin: 0.5rem;
+        }
+    }
+`
+
 export const Attribution = styled.div`
     font-size: 11px; text-align: center;
     a { color: hsl(228, 45%, 44%); }
